9fabb80aeb83af9170eeefdc3aa20809a7ad0f72
Henry Zhang <henryzhang9802@gmail.com>
2019-11-02 00:08:18 +0000

    Move to vue componet

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,14 @@
+Vue.component('bubble', {
+    props: ['sender', 'content'],
+    template: `
+    <div class="conversation">
+        <img src="img/icon.png" v-if="sender=='server'" class="icon">
+        <div :class="{bubble:true, bubble_server:sender=='server', bubble_client:sender=='client'}">{{content}}</div>
+    </div>
+    `
+  })
+
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -6,12 +17,4 @@ var app = new Vue({
     methods: {
       
     }
-  })
-
-
-  Vue.component('bubble', {
-    props: ['sender', 'content'],
-    template: `
-        <div><div>  
-    `
   })
