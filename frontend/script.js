466eef1d01bcca68361d92f323e9ca54d03b4771
Henry Zhang <henryzhang9802@gmail.com>
2019-11-02 03:17:36 +0000

    Add bounce animation

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,13 @@
 Vue.component('bubble', {
     props: ['sender', 'content','isloading'],
     template: `
-    <div class="conversation" :class="{client_right_align:sender=='client'}">
-        <img src="img/icon.png" v-if="sender!='client'" class="icon">
-        <div :class="{bubble:true, bubble_server:sender!='client', bubble_client:sender=='client', bubble_loading:isloading}">
-            {{isloading ? 'typing...' : content}}
+        <div class="conversation" :class="{client_right_align:sender=='client'}" >
+            <img src="img/icon.png" v-if="sender!='client'" class="icon">
+                <div :class="{bubble:true, bubble_server:sender!='client', bubble_client:sender=='client', bubble_loading:isloading, bubble_bounce:!isloading && sender!='client'}">
+                    {{isloading ? 'typing...' : content}}
+                </div>
         </div>
-    </div>
+
     `
   })
 
@@ -43,7 +44,7 @@ var app = new Vue({
         setTimeout(() => {
             app.bubbleList.pop()
             app.bubbleList.push(this.questions[this.currentQ]);
-        }, 3000);
+        }, 1500);
         
     }
 
