2babb0f4f4444f6e3663ba8faeca7cbe054b6198
Henry Zhang <henryzhang9802@gmail.com>
2019-11-02 04:58:00 +0000

    Add animations

--- frontend/script.js
+++ frontend/script.js
@@ -24,6 +24,7 @@ var app = new Vue({
         submitForm: function(){
             this.editing = false
             this.bubbleList.push({content:this.message, isclient:true})
+            window.scrollTo({ top: 9000, behavior: 'smooth' })
             if(!this.userId){
                 registration.handleCurrent(this.message);
             }
@@ -37,9 +38,12 @@ var app = new Vue({
     questions: [
                {content: "Hello! How can I help you with your uni application today?", bindData: "Query"},
                {content: "No problem! First, could you tell me which country are you from?", bindData: "HomeCountry"},
-               {content: "Which country do you want to study in? It can be more than one!", bindData: "TargetCountry"},
-               {content: "What are the subjects you are currently studying? This will help us find your perfect match!", bindData: "Subject"},
-               {content: "Great. Now the tricky one, what is your estimated grade?", bindData: "Grade"},
+               {content: "Which country do you want to study in? It can be more than one!", continue: true},
+               {content: "Just type and use comma to seperate them.", bindData: "TargetCountry"},
+               {content: "What are the subjects you are currently studying? This will help us find your perfect match!", continue: true},
+               {content: "Just type and use comma to seperate them.", bindData: "Subject"},
+               {content: "Great Choice!", continue: true},
+               {content: "Now the tricky part, what is your estimated grade of each subject?", bindData: "Grade"},
                {content: "Thanks. And what is the education system you are in?", bindData: "EducationType"},
                {content: "Lastly, what's your preferred finance range?", bindData: "FinanceRange"},
                {content: "And Finally, what's your age?", bindData: "Age"},
@@ -56,18 +60,31 @@ var app = new Vue({
         
     },
 
+    push:function(){
+        this.currentQ++;
+        app.bubbleList.push(this.questions[this.currentQ]);
+        window.scrollTo({ top: 9000, behavior: 'smooth' })
+    },
+
     goToNextQuestion: function(){
         app.bubbleList.push({isloading:true})
-        window.scrollTo(0,document.body.scrollHeight);
-        this.currentQ++;
+        window.scrollTo({ top: 9000, behavior: 'smooth' })
+        
         //get typing effect
         setTimeout(() => {
             app.bubbleList.pop()
-            app.bubbleList.push(this.questions[this.currentQ]);
+
+            let that =this
+            let timer = setInterval(function () {
+                that.push();
+                if (!that.questions[that.currentQ].continue) {
+                    clearInterval(timer);
+                }
+            }, 200);
             
             setTimeout(() => {app.editing = true;}, 400)
             setTimeout(() => {document.getElementById("msg").focus()}, 600)
-        }, 1500);
+        }, 1200);
         
     }
 
