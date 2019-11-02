05f21de1f2d00028f0be3f882f4864ebd7914c6f
Henry Zhang <henryzhang9802@gmail.com>
2019-11-02 01:48:49 +0000

    Add typing animation

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,11 @@
 Vue.component('bubble', {
-    props: ['sender', 'content'],
+    props: ['sender', 'content','isloading'],
     template: `
     <div class="conversation" :class="{client_right_align:sender=='client'}">
         <img src="img/icon.png" v-if="sender=='server'" class="icon">
-        <div :class="{bubble:true, bubble_server:sender=='server', bubble_client:sender=='client'}">{{content}}</div>
+        <div :class="{bubble:true, bubble_server:sender=='server', bubble_client:sender=='client', bubble_loading:isloading}">
+            {{isloading ? 'typing...' : content}}
+        </div>
     </div>
     `
   })
